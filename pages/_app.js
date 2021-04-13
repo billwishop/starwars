9267561caaeb486eab7d577560eed39d590eb135
billwishop <willbishop.hello@gmail.com>
2021-04-13 12:12:25 -0500

    The search bar is filtering the list and rendering the appropriate characters.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+// import { ContextProvider } from '../components/CharacterContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+      <Component {...pageProps} />
+
+  )
 }
 
 export default MyApp
