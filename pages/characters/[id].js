1ae634463652a2243c201cf7c702db3089a118c8
billwishop <willbishop.hello@gmail.com>
2021-04-13 19:57:45 -0500

    The paths are being retrieved and routes are now being dynamically populated.

--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
+import { getAllCharacterIds, getCharacterData } from '../../lib/characters'
 
-export default function FirstPost() {
+export default function CharacterDetails({ characterData }) {
+    console.log(characterData)
     return (
         <>
-            <h1>First Post</h1>
+            <h1>{characterData.result.properties.name}</h1>
             <h2>
             <Link href="/">
                 <a>Back to home</a>
@@ -11,4 +13,22 @@ export default function FirstPost() {
             </h2>
         </>
     )
+}
+
+export async function getStaticPaths() {
+    const paths = await getAllCharacterIds()
+        return {
+            paths,
+            fallback: true
+        }
+    
+}
+
+export async function getStaticProps({ params }) {
+    const characterData = await getCharacterData(params.id)
+    return {
+        props: {
+            characterData
+        }
+    }
 }
